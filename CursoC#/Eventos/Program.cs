083b25243ce5e02cd6f3a7bb55fa16d73dce8eeb
Gerardo Ku <gerardo.ku@blueoceantech.com.mx>
2020-01-27 12:53:14 -0600

    primera versión
    primera versión

---
+++ CursoC#/Eventos/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.IO;
+
+namespace Eventos
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            StreamReader file = new StreamReader(@"C:\Usuarios\gerardo.ku\Desktop\Eventos.txt");
+           
+            Propiedades _propiedades = new Propiedades();
+            Console.WriteLine("Evento:");
+            _propiedades.SetNombre(Console.ReadLine());
+            //Console.WriteLine("Fecha Evento: ");
+
+            //_propiedades.SetFecha(Console.ReadLine());
+           // Console.WriteLine(_propiedades.getFecha());
+            Console.ReadKey();
+
+            Archivo.EscribirArchivo(_propiedades.getNombre(), _propiedades.getFecha(), file.ToString());
+            Archivo.LeeArchivo(file.ToString());          
+        }
+
+       
+    }
+}
